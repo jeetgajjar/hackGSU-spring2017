f44ebcbe53eaed4c64081c30de0a4159ad965aae
Leo Chen <leochen@Leos-MacBook-Pro.local>
2017-04-02 00:35:35 -0400

    ya

--- src/main/java/view/ViewImagePage.java
+++ src/main/java/view/ViewImagePage.java
@@ -1,23 +1,32 @@
 import java.io.File;
 import java.io.IOException;
-
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class ViewImagePage {
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
+        JLabel player;
+
+        //rep windows on screen
+        //jpanel class is for more complex interfaces
+        JFrame mainFrame = new JFrame("Game Maker");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         try {
-            f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("../../../folderImage/soccerField.png")))));
+            mainFrame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("../../../folderImage/soccerField.png")))));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        f.pack();
-        f.setVisible(true);
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
     }
 
 }
